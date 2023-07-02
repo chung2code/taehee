bb6d8cbd9bce97e6c917b1d6dfee6b167cd99974
chung2code <anthdny7@gmail.com>
2023-07-03 01:43:02 +0900

    20230702subject

---
+++ src/Person.java
@@ -0,0 +1,54 @@
+public class Person {
+
+private String name;
+private String address;
+private int phoneNumber;
+private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        person.getAddress();
+        person.getName();
+        person.getAge();
+        person.getPhoneNumber();
+
+
+
+
+
+    }
+
+}
